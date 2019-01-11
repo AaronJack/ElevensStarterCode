6395126a9e6759bceb62717837c1af79d30dc785
aaronj <ajack9601@bths.edu>
2019-01-11 11:27:53 -0500

    Almost Famous

--- src/Activity4/DeckTester.java
+++ src/Activity4/DeckTester.java
@@ -12,7 +12,7 @@ public class DeckTester {
     public static void main(String[] args) {
         String[] ranks = {"jack", "queen", "king"};
         String[] suits = {"blue", "red"};
-        int[] pointValues = {11, 12, 13};
+        int[] pointValues = {11, 12, 13};//
         Deck d = new Deck(ranks, suits, pointValues);
 
         System.out.println("**** Original Deck Methods ****");
